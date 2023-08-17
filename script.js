55bf787f5a426db8fb1cd96f81380e76c5e3ce54
Nick <nickjoemaz@gmail.com>
2023-08-17 19:09:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var redscore = 0;
+var output = document.getElementById('output');
+
+function Scorered()
+{
+    redscore = redscore + 1;
+    output.innerHTML = redscore;
+}
+
+
+var bluescore = 0;
+var outputtwo = document.getElementById('outputtwo');
+
+function Scoreblue()
+{
+    bluescore = bluescore + 1;
+    outputtwo.innerHTML = bluescore;
+}
+
